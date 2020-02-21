c14655b4d2a8c25c03a2435cd2c78c53e7f44a9f
il-hubspot <48827131+il-hubspot@users.noreply.github.com>
2020-02-21 11:33:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-    $(".content-13-v2").each(function() {
+    $(".content-11").each(function() {
         var o = $(this);
         if (o.hasClass("init")) {
             return;
@@ -7,20 +7,28 @@ jQuery(document).ready(function($) {
         o.addClass("init");
       
       	var style = "";
-      
-      	var newId = "id-" + Math.random().toString().slice(2);
-        o.attr("id", newId);
-      
-      	var btnColor = $(this).find(".js-form-btn-color").data("form-btn-color");
-        if (btnColor && btnColor !== "#16a085") {
-            style += "#" + newId + ".content-13 .hs_submit input[type='submit'] { background-color: " + btnColor + "; } \n";
-            $("<style type='text/css'/>").html(style).appendTo("head");
-        }
-      
-      	var btnHoverColor = $(this).find(".js-form-btn-color").data("form-btn-hover-color");
-        if (btnHoverColor && btnHoverColor !== "#24deb4") {
-            style += "#" + newId + ".content-13 .hs_submit input[type='submit']:hover, #" + newId + ".content-13 .hs_submit input[type='submit']:focus, #" + newId + ".content-13 .hs_submit input[type='submit']:active{ background-color: " + btnHoverColor + "; } \n";
-            $("<style type='text/css'/>").html(style).appendTo("head");
-        }
+
+        o.find(".js-btn-color").each(function(){
+            var btnType = $(this).data('btn-type');
+            var btnColor = $(this).data('btn-color');
+            var btnHoverColor = $(this).data('btn-hover-color');
+            
+            var btnId = "id-" + Math.random().toString().slice(2);
+            $(this).attr("id", btnId);
+
+            $(this).removeAttr('style');
+
+             if(btnType === 'normal') {
+                style += "#" + btnId + ".btn { background-color: " + btnColor + "; color: #ffffff; } \n";
+                style += "#" + btnId + ".btn:hover, #" + btnId + ".btn:focus, #" + btnId + ".btn:active { background-color: " + btnHoverColor + "; } \n";
+            }
+
+            if(btnType === 'clear') {
+                style += "#" + btnId + ".btn { background-color: transparent; border: 2px solid " + btnColor + "; color: " + btnColor + "; } \n";
+                style += "#" + btnId + ".btn:hover, #" + btnId + ".btn:focus, #" + btnId + ".btn:active { background-color: " + btnColor + "; color: #ffffff; } \n";
+            }
+        });
+
+        $("<style type='text/css'/>").html(style).appendTo("head");
     });
 });
